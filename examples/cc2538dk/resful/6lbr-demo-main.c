b72c1d1041bf93afbd9ae016c74ced085644985c
Arex <zjfbob@gmail.com>
2017-04-23 15:02:49 +0800

    add restful

---
+++ examples/cc2538dk/resful/6lbr-demo-main.c
@@ -0,0 +1,268 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "contiki.h"
+#include "contiki-net.h"
+#include "rest-engine.h"
+
+#if PLATFORM_HAS_BUTTON
+#include "dev/button-sensor.h"
+#endif
+
+#define DEBUG 0
+#if DEBUG
+#include <stdio.h>
+#define PRINTF(...) printf(__VA_ARGS__)
+#define PRINT6ADDR(addr) PRINTF("[%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x]", ((uint8_t *)addr)[0], ((uint8_t *)addr)[1], ((uint8_t *)addr)[2], ((uint8_t *)addr)[3], ((uint8_t *)addr)[4], ((uint8_t *)addr)[5], ((uint8_t *)addr)[6], ((uint8_t *)addr)[7], ((uint8_t *)addr)[8], ((uint8_t *)addr)[9], ((uint8_t *)addr)[10], ((uint8_t *)addr)[11], ((uint8_t *)addr)[12], ((uint8_t *)addr)[13], ((uint8_t *)addr)[14], ((uint8_t *)addr)[15])
+#define PRINTLLADDR(lladdr) PRINTF("[%02x:%02x:%02x:%02x:%02x:%02x]", (lladdr)->addr[0], (lladdr)->addr[1], (lladdr)->addr[2], (lladdr)->addr[3], (lladdr)->addr[4], (lladdr)->addr[5])
+#else
+#define PRINTF(...)
+#define PRINT6ADDR(addr)
+#define PRINTLLADDR(addr)
+#endif
+
+#if SHELL
+#include "shell.h"
+#include "serial-shell.h"
+#include "dev/serial-line.h"
+#ifdef CONTIKI_TARGET_Z1
+#include "dev/uart0.h"
+#else
+#include "dev/uart1.h"
+#endif
+#include "shell-6lbr.h"
+#endif
+
+#ifdef CONTIKI_TARGET_ECONOTAG
+#include "maca.h"
+#endif
+
+#ifdef CONTIKI_TARGET_CC2538DK
+#include "i2c.h"
+#endif
+
+#if WITH_TINYDTLS
+#include "dtls.h"
+#endif
+
+#if WITH_DTLS_ECHO
+#include "dtls-echo.h"
+#endif
+
+#if WITH_CETIC_6LN_NVM
+#include "nvm-config.h"
+#endif
+
+#if WITH_COAPSERVER
+#include "coap-server.h"
+#endif
+
+#if WITH_IPSO_APP_FW
+#include "ipso-app-fw.h"
+#endif
+
+#if WITH_LWM2M
+#include "lwm2m.h"
+#endif
+
+/*
+ * Resources to be activated need to be imported through the extern keyword.
+ * The build system automatically compiles the resources in the corresponding sub-directory.
+ */
+extern resource_t
+  res_hello,
+  res_mirror,
+  res_chunks,
+  res_separate,
+  res_push,
+  res_event,
+  res_sub,
+  res_b1_sep_b2;
+#if PLATFORM_HAS_LEDS
+extern resource_t res_leds, res_toggle;
+#endif
+#if PLATFORM_HAS_LIGHT
+#include "dev/light-sensor.h"
+extern resource_t res_light;
+#endif
+
+PROCESS(er_example_server, "Erbium Example Server");
+PROCESS(demo_6lbr_process, "6LBR Demo");
+
+#if WEBSERVER
+PROCESS_NAME(web_sense_process);
+PROCESS_NAME(webserver_nogui_process);
+#endif
+#if UDPCLIENT
+PROCESS_NAME(udp_client_process);
+#endif
+
+/*---------------------------------------------------------------------------*/
+#define INIT_USER_MOD(module) \
+  extern void module##_init(); \
+  module##_init();
+
+void
+start_apps(void)
+{
+#if UDPCLIENT
+  process_start(&udp_client_process, NULL);
+#endif
+
+#if WEBSERVER
+  process_start(&web_sense_process, NULL);
+  process_start(&webserver_nogui_process, NULL);
+#endif
+
+#if WITH_COAPSERVER
+  coap_server_init();
+#endif
+
+#if WITH_IPSO_APP_FW
+  ipso_app_fw_init();
+#endif
+
+#if WITH_LWM2M
+  lwm2m_init();
+#endif
+
+#if !IGNORE_CETIC_CONTIKI_PLATFORM && WITH_COAPSERVER
+  extern void contiki_platform_resources_init();
+  contiki_platform_resources_init();
+#endif
+
+#if defined CETIC_6LN_PLATFORM_CONF && WITH_COAPSERVER
+  extern void platform_resources_init();
+  platform_resources_init();
+#endif
+
+#if WITH_DTLS_ECHO
+  process_start(&dtls_echo_server_process, NULL);
+#endif
+
+#ifdef USER_MODULES
+  USER_MODULES;
+#endif
+}
+
+PROCESS_THREAD(demo_6lbr_process, ev, data)
+{
+  PROCESS_BEGIN();
+
+#if WITH_CETIC_6LN_NVM
+  load_nvm_config();
+#endif
+
+#ifdef CONTIKI_TARGET_ECONOTAG
+  set_channel(RF_CHANNEL - 11);
+#endif
+
+#ifdef CONTIKI_TARGET_CC2538DK
+#if ENABLE_I2C
+  i2c_init(I2C_SDA_PORT, I2C_SDA_PIN, I2C_SCL_PORT, I2C_SCL_PIN, I2C_SCL_FAST_BUS_SPEED);
+#endif
+#endif
+
+  #if WITH_TINYDTLS
+  dtls_init();
+#endif
+
+#if SHELL
+#ifdef CONTIKI_TARGET_Z1
+  uart0_set_input(serial_line_input_byte);
+#elif defined CONTIKI_TARGET_COOJA
+  rs232_set_input(serial_line_input_byte);
+#else
+  uart1_set_input(serial_line_input_byte);
+#endif
+  serial_line_init();
+
+  serial_shell_init();
+  shell_ping_init();
+  shell_6lbr_init();
+#endif
+#if !WITH_DELAY_IP
+  start_apps();
+#endif
+
+  PROCESS_END();
+}
+/*---------------------------------------------------------------------------*/
+
+PROCESS_THREAD(er_example_server, ev, data)
+{
+  PROCESS_BEGIN();
+
+  PROCESS_PAUSE();
+
+  PRINTF("Starting Erbium Example Server\n");
+
+#ifdef RF_CHANNEL
+  PRINTF("RF channel: %u\n", RF_CHANNEL);
+#endif
+#ifdef IEEE802154_PANID
+  PRINTF("PAN ID: 0x%04X\n", IEEE802154_PANID);
+#endif
+
+  PRINTF("uIP buffer: %u\n", UIP_BUFSIZE);
+  PRINTF("LL header: %u\n", UIP_LLH_LEN);
+  PRINTF("IP+UDP header: %u\n", UIP_IPUDPH_LEN);
+  PRINTF("REST max chunk: %u\n", REST_MAX_CHUNK_SIZE);
+
+  /* Initialize the REST engine. */
+  rest_init_engine();
+
+  /*
+   * Bind the resources to their Uri-Path.
+   * WARNING: Activating twice only means alternate path, not two instances!
+   * All static variables are the same for each URI path.
+   */
+  rest_activate_resource(&res_hello, "test/hello");
+/*  rest_activate_resource(&res_mirror, "debug/mirror"); */
+/*  rest_activate_resource(&res_chunks, "test/chunks"); */
+/*  rest_activate_resource(&res_separate, "test/separate"); */
+  rest_activate_resource(&res_push, "test/push");
+/*  rest_activate_resource(&res_event, "sensors/button"); */
+/*  rest_activate_resource(&res_sub, "test/sub"); */
+/*  rest_activate_resource(&res_b1_sep_b2, "test/b1sepb2"); */
+#if PLATFORM_HAS_LEDS
+/*  rest_activate_resource(&res_leds, "actuators/leds"); */
+  rest_activate_resource(&res_toggle, "actuators/toggle");
+#endif
+#if PLATFORM_HAS_LIGHT
+  rest_activate_resource(&res_light, "sensors/light"); 
+  SENSORS_ACTIVATE(light_sensor);  
+#endif
+/*
+#if PLATFORM_HAS_BATTERY
+  rest_activate_resource(&res_battery, "sensors/battery");  
+  SENSORS_ACTIVATE(battery_sensor);  
+#endif
+#if PLATFORM_HAS_RADIO
+  rest_activate_resource(&res_radio, "sensors/radio");  
+  SENSORS_ACTIVATE(radio_sensor);  
+#endif
+#if PLATFORM_HAS_SHT11
+  rest_activate_resource(&res_sht11, "sensors/sht11");  
+  SENSORS_ACTIVATE(sht11_sensor);  
+#endif
+*/
+
+  /* Define application-specific events here. */
+  while(1) {
+    PROCESS_WAIT_EVENT();
+#if PLATFORM_HAS_BUTTON
+    if(ev == sensors_event && data == &button_sensor) {
+      PRINTF("*******BUTTON*******\n");
+
+      /* Call the event_handler for this application-specific event. */
+      res_event.trigger();
+
+      /* Also call the separate response example handler. */
+      res_separate.resume();
+    }
+#endif /* PLATFORM_HAS_BUTTON */
+  }                             /* while (1) */
+
+  PROCESS_END();
+}
---
+++ examples/cc2538dk/resful/project-conf.h
@@ -0,0 +1,306 @@
+/*
+ * Copyright (c) 2013, CETIC.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+/**
+ * \file
+ *         6LBR-Demo Project Configuration
+ * \author
+ *         6LBR Team <6lbr@cetic.be>
+ */
+
+#ifndef CETIC_6LBR_DEMO_PROJECT_CONF_H
+#define CETIC_6LBR_DEMO_PROJECT_CONF_H
+
+/* Remove rf_channel forced by platform */
+#ifndef USER_RF_CHANNEL
+#undef RF_CHANNEL
+#endif
+
+/* Undefine hardcoded platform configuration */
+#undef QUEUEBUF_CONF_NUM
+#undef UIP_CONF_BUFFER_SIZE
+#undef UIP_CONF_RECEIVE_WINDOW
+#undef WEBSERVER_CONF_CFS_CONNS
+#undef WEBSERVER_CONF_CFS_PATHLEN
+#undef NETSTACK_CONF_MAC
+#undef NETSTACK_CONF_RDC
+#undef SKY_CONF_MAX_TX_POWER
+#undef RPL_CONF_INIT_LINK_METRIC
+#undef UIP_CONF_DS6_NBR_NBU
+#undef UIP_CONF_MAX_ROUTES
+
+/* include the project config */
+#ifdef USER_PROJECT_CONF_H
+#include USER_PROJECT_CONF_H
+#endif
+
+/* Platform related configuration */
+#if !IGNORE_CETIC_CONTIKI_PLATFORM
+#include "platform/contiki/6ln-conf-contiki.h"
+#endif
+
+#ifdef CETIC_6LN_PLATFORM_CONF
+#include CETIC_6LN_PLATFORM_CONF
+#endif
+
+/*---------------------------------------------------------------------------*/
+/* Radio                                                                     */
+/*---------------------------------------------------------------------------*/
+
+#ifndef IEEE802154_CONF_PANID
+#define IEEE802154_CONF_PANID 0xABCD
+#endif
+
+#ifndef RF_CHANNEL
+#define RF_CHANNEL 26
+#endif
+
+#define CC2538_RF_CONF_CHANNEL RF_CHANNEL
+#define CC26XX_RF_CONF_CHANNEL RF_CHANNEL
+#undef CC2420_CONF_CHANNEL
+#define CC2420_CONF_CHANNEL RF_CHANNEL
+
+/*---------------------------------------------------------------------------*/
+/* Security                                                                  */
+/*---------------------------------------------------------------------------*/
+
+#if WITH_LLSEC
+
+#undef NETSTACK_LLSEC
+#define NETSTACK_LLSEC noncoresec_driver
+
+#undef LLSEC802154_CONF_SECURITY
+#define LLSEC802154_CONF_SECURITY 1
+#ifndef LLSEC802154_CONF_SECURITY_LEVEL
+#define LLSEC802154_CONF_SECURITY_LEVEL 6
+#endif
+
+#undef AES_128_CONF
+#define AES_128_CONF aes_128_driver
+
+#define LLSEC_ANTIREPLAY_ENABLED 1
+#define LLSEC_REBOOT_WORKAROUND_ENABLED 1
+
+#else
+
+#undef NETSTACK_LLSEC
+#define NETSTACK_LLSEC nullsec_driver
+
+#endif
+
+/*---------------------------------------------------------------------------*/
+/* 6LoWPAN                                                                   */
+/*---------------------------------------------------------------------------*/
+
+#define CETIC_6LBR_6LOWPAN_CONTEXT_0  { 0xAA, 0xAA, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }
+
+/*---------------------------------------------------------------------------*/
+/* COAP                                                                      */
+/*---------------------------------------------------------------------------*/
+
+#define COAP_SERVER_PORT 5683
+
+/* Uncomment to remove /.well-known/core resource to save code */
+//#define WITH_WELL_KNOWN_CORE            0
+
+/* COAP content type definition */
+#ifndef COAP_CONF_DATA_FORMAT
+#define COAP_CONF_DATA_FORMAT coap_data_format_text
+#endif
+#define REST_MAX_CHUNK_SIZE     64
+
+/*---------------------------------------------------------------------------*/
+/* DNS                                                                      */
+/*---------------------------------------------------------------------------*/
+
+#define RESOLV_CONF_SUPPORTS_DNS_SD   0
+
+#define RESOLV_CONF_SUPPORTS_MDNS     0
+
+/*---------------------------------------------------------------------------*/
+/* UDP-CLIENT                                                                */
+/*---------------------------------------------------------------------------*/
+
+#define CETIC_6LBR_NODE_INFO_PORT 3000
+
+/* Report LQI and RSSI only for parent node */
+#define UDP_CLIENT_STORE_RADIO_INFO 1
+
+/*---------------------------------------------------------------------------*/
+/* DTLS                                                                      */
+/*---------------------------------------------------------------------------*/
+
+#define DTLS_PEER_MAX 3
+
+#define DTLS_CONF_CONN_TIMEOUT 10
+
+//#define DTLS_CONF_PSK_KEY "secretPSK"
+//#define DTLS_CONF_PSK_KEY_LENGTH 9
+
+/*---------------------------------------------------------------------------*/
+/* DTLS ECHO                                                                 */
+/*---------------------------------------------------------------------------*/
+
+#define DTLS_ECHO_PORT 20220
+
+/*---------------------------------------------------------------------------*/
+/* UIP Buffers                                                               */
+/*---------------------------------------------------------------------------*/
+
+#if !UIP_CONF_IPV6_RPL
+#undef UIP_CONF_ROUTER
+#define UIP_CONF_ROUTER            0
+#endif
+
+#ifndef QUEUEBUF_CONF_NUM
+#define QUEUEBUF_CONF_NUM          5
+#endif
+
+#ifndef UIP_CONF_BUFFER_SIZE
+#if CONTIKI_TARGET_SKY || CONTIKI_TARGET_Z1
+#define UIP_CONF_BUFFER_SIZE    260
+#else
+#define UIP_CONF_BUFFER_SIZE    600
+#endif
+#endif
+
+#ifndef UIP_CONF_RECEIVE_WINDOW
+#define UIP_CONF_RECEIVE_WINDOW  60
+#endif
+
+/*---------------------------------------------------------------------------*/
+/* WEBSERVER                                                                 */
+/*---------------------------------------------------------------------------*/
+
+#ifndef WEBSERVER_CONF_CFS_CONNS
+#define WEBSERVER_CONF_CFS_CONNS 2
+#endif
+
+/* Reserve space for a file name (default is to not use file name) */
+#ifndef WEBSERVER_CONF_CFS_PATHLEN
+#define WEBSERVER_CONF_CFS_PATHLEN 80
+#endif
+
+/*---------------------------------------------------------------------------*/
+/* RADIO                                                                     */
+/*---------------------------------------------------------------------------*/
+
+#ifndef NETSTACK_CONF_MAC
+#define NETSTACK_CONF_MAC     		csma_driver
+#endif
+
+#ifndef NETSTACK_CONF_RDC
+#define NETSTACK_CONF_RDC     		nullrdc_driver
+#endif
+
+#ifndef SKY_CONF_MAX_TX_POWER
+#define SKY_CONF_MAX_TX_POWER 	31
+#endif
+
+#if CONTIKI_TARGET_ECONOTAG
+#undef NULLRDC_CONF_802154_AUTOACK
+#define NULLRDC_CONF_802154_AUTOACK_HW     1
+#else
+#undef NULLRDC_CONF_802154_AUTOACK
+#define NULLRDC_CONF_802154_AUTOACK	1
+#endif
+
+/*---------------------------------------------------------------------------*/
+/* RPL & Network                                                             */
+/*---------------------------------------------------------------------------*/
+
+#ifndef RPL_CONF_INIT_LINK_METRIC
+#define RPL_CONF_INIT_LINK_METRIC			2
+#endif
+
+#define RPL_MAX_DAG_PER_INSTANCE	2
+#define RPL_MAX_INSTANCES		1
+
+// Always use infinite upward route
+#define RPL_CONF_DEFAULT_ROUTE_INFINITE_LIFETIME    1
+
+/* Z1 platform has limited RAM */
+
+#if defined CONTIKI_TARGET_Z1
+
+#define RPL_CONF_MAX_PARENTS_PER_DAG    12
+#define NEIGHBOR_CONF_MAX_NEIGHBORS     12
+
+#ifndef UIP_CONF_DS6_NBR_NBU
+#define UIP_CONF_DS6_NBR_NBU     12
+#endif
+
+#ifndef UIP_CONF_MAX_ROUTES
+#define UIP_CONF_MAX_ROUTES   12
+#endif
+
+#else
+
+#define RPL_CONF_MAX_PARENTS_PER_DAG    24
+#define NEIGHBOR_CONF_MAX_NEIGHBORS     24
+
+#ifndef UIP_CONF_DS6_NBR_NBU
+#define UIP_CONF_DS6_NBR_NBU     24
+#endif
+
+#ifndef UIP_CONF_MAX_ROUTES
+#define UIP_CONF_MAX_ROUTES   24
+#endif
+
+#undef UIP_CONF_ND6_SEND_NA
+#define UIP_CONF_ND6_SEND_NA   0
+
+#endif
+
+/*---------------------------------------------------------------------------*/
+/* Init                                                                      */
+/*---------------------------------------------------------------------------*/
+
+
+#define UIP_DS6_CONF_NO_STATIC_ADDRESS 1
+
+/*---------------------------------------------------------------------------*/
+/* Miscellaneous                                                             */
+/*---------------------------------------------------------------------------*/
+
+/* Disable ENERGEST to save code */
+
+#if defined CONTIKI_TARGET_SKY || (defined CONTIKI_TARGET_Z1 && ! MSP430_20BITS)
+#undef ENERGEST_CONF_ON
+#define ENERGEST_CONF_ON 0
+#endif
+
+#undef UART1_CONF_RX_WITH_DMA
+#define UART1_CONF_RX_WITH_DMA 0
+
+#define SICSLOWPAN_CONF_ADDR_CONTEXT_0 \
+    uint8_t context_0[8] = CETIC_6LBR_6LOWPAN_CONTEXT_0; \
+  memcpy(addr_contexts[0].prefix, context_0, sizeof(addr_contexts[0].prefix));
+
+#endif /* CETIC_6LBR_DEMO_PROJECT_CONF_H */
